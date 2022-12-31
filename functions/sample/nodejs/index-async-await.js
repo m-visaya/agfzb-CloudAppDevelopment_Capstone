5299e59690374ed8d2c155eed03dcd35049b7e21
Marco Visaya <marco.visaya527@gmail.com>
2022-12-31 14:06:15 +0800

    Update serverless function for fetching all dealers

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -1,21 +1,44 @@
-/**
- * Get all dealerships
- */
-
-const { CloudantV1 } = require('@ibm-cloud/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+const { CloudantV1 } = require("@ibm-cloud/cloudant");
+const { IamAuthenticator } = require("ibm-cloud-sdk-core");
 
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-      const cloudant = CloudantV1.newInstance({
-          authenticator: authenticator
+  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
+  const cloudant = CloudantV1.newInstance({
+    authenticator: authenticator,
+  });
+  cloudant.setServiceUrl(params.COUCH_URL);
+  let response = null;
+  try {
+    if (params.state) {
+      const selector = {
+        state: {
+          $eq: params.state,
+        },
+      };
+      response = await cloudant.postFind({
+        db: "dealerships",
+        selector: selector,
       });
-      cloudant.setServiceUrl(params.COUCH_URL);
-      try {
-        let dbList = await cloudant.getAllDbs();
-        return { "dbs": dbList.result };
-      } catch (error) {
-          return { error: error.description };
-      }
+      return { response: response.result.docs };
+    } else if (params.dealerId) {
+      const selector = {
+        id: {
+          $eq: parseInt(params.idealerId),
+        },
+      };
+      response = await cloudant.postFind({
+        db: "dealerships",
+        selector: selector,
+      });
+      return { response: response.result.docs };
+    } else {
+      response = await cloudant.postAllDocs({
+        db: "dealerships",
+        includeDocs: true,
+      });
+    }
+    return response.result;
+  } catch (error) {
+    return { error: error.description };
+  }
 }
-
